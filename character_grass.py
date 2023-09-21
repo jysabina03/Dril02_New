dccb0b4e7f7b5764523f5887f1e740ee45a5c56c
2DGP class <129085079+808Jade@users.noreply.github.com>
2023-09-21 11:44:32 +0900

    load image
    이미지 로드&기본 출력

--- character_grass.py
+++ character_grass.py
@@ -4,4 +4,11 @@ open_canvas()
 
 # fill here
 
+character = load_image('character.png')
+grass = load_image('grass.png')
+clear_canvas_now()
+
+grass.draw_now(400,30)
+character.draw_now(400,90)
+    
 close_canvas()
